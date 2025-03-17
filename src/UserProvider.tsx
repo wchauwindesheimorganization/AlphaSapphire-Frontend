8701de1ecd46519a6c36f77acc25275f5c6ba682
Giofenna <100773460+Giofenna@users.noreply.github.com>
2025-03-17 16:13:32 +0100

    Wrote tests for login behaviour for the accountprovider

--- src/UserProvider.tsx
+++ src/UserProvider.tsx
@@ -1,26 +1,30 @@
 import { UserContext } from "./UserContext";
-import { useState, useEffect } from "react";
+import { useState, useEffect, act } from "react";
 import React from "react";
-import axios from "axios";
-import { msalInstance } from "./main";
-import { api } from "./Api";
 import { getActiveUser } from "./api/userApi";
+import { useMsal } from "@azure/msal-react";
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const activeAccount = msalInstance?.getActiveAccount();
+  const msalInstance = useMsal();
+  const activeAccount = msalInstance.instance.getActiveAccount();
+
   const [account, setAccount] = useState<Object | undefined>(undefined);
   const [error, setError] = useState<string | null>(null);
-  console.log(account);
   useEffect(() => {
     const acquireUser = async () => {
       if (activeAccount) {
-        getActiveUser().then((response) => {
-          setAccount(response);
-        });
+        getActiveUser()
+          .then((response) => {
+            setAccount(response);
+          })
+          .catch((error) => {
+            setError(error.message);
+          });
       }
     };
 
     acquireUser();
   }, []);
+  console.log([activeAccount, account, "test"]);
   return (
     <UserContext.Provider value={{ account: account }}>
       {account && children}
